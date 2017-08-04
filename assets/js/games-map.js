e919cbb55f9212983bf1d98f3aa6ae607e32249d
Gregory McIntyre <greg@gregorymcintyre.com>
2017-08-04 11:31:09 +1000

    Click on the map to initiate a search

--- assets/js/games-map.js
+++ assets/js/games-map.js
@@ -21,13 +21,65 @@ function initMap(mapDiv) {
     .addTo(map);
 
   fetch("/api/games")
-    .then(json => json.json())
+    .then(body => body.json())
     .then(response => {
       response.data.forEach(game => {
         L.marker(game.coordinates)
           .addTo(markers)
           .bindPopup(game.name);
       });
-      map.fitBounds(markers.getBounds());
-    })
+      map.fitBounds(markers.getBounds(), {padding: [10, 10]});
+    });
+
+  const searchMarker = L.circle()
+    .setRadius(10000); // metres
+
+  const form = document.querySelector('.games-form');
+
+  const searchRadiusMetres = () => {
+    const value = parseInt(form.querySelector('.distance-value').value);
+    const unit = form.querySelector('.distance-unit').value;
+    console.log(value, unit);
+    if (unit === "km") {
+      return value * 1000;
+    } else if (unit === "mi") {
+      return value * 1609;
+    } else {
+      return 10000; // default = 10 km
+    }
+  }
+
+  const onClick = (e) => {
+    L.DomEvent.stopPropagation(e);
+
+    // update search marker
+    const searchRadius = searchRadiusMetres();
+    searchMarker
+      .setLatLng(e.latlng)
+      .setRadius(searchRadius)
+      .addTo(map);
+
+    // update place input
+    const locationInput = form.querySelector('.location-input');
+    locationInput.value = e.latlng.lat + ',' + e.latlng.lng;
+
+    // request games nearby
+    const searchParams = new URLSearchParams();
+    searchParams.append("lat", e.latlng.lat);
+    searchParams.append("lng", e.latlng.lng);
+    searchParams.append("m", searchRadius);
+    const url = "/api/games?" + searchParams.toString();
+    fetch(url)
+      .then(body => body.json())
+      .then(response => {
+        response.data.forEach(game => {
+          L.marker(game.coordinates)
+            .addTo(markers)
+            .bindPopup(game.name);
+        });
+        map.fitBounds(markers.getBounds(), {padding: [10, 10]});
+      });
+  }
+
+  map.on('click', onClick);
 }
